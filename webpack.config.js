75f969f01a75c7b4f00e0ab22d88ab22052b5e5f
Jacob Westman <hello@jacobwestman.com>
2021-05-30 19:05:54 +0200

    added webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,31 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: "babel-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          publicPath: "images",
+        },
+      },
+    ],
   },
   plugins: [
     new CopyPlugin({
-      patterns: [
-        { from: "public"},
-      ],
+      patterns: [{ from: "public" }],
     }),
-  ]
+  ],
 };
