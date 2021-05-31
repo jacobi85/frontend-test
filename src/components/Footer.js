c5a5de8207eb8466e33eff9e95db6c1869888502
Jacob Westman <hello@jacobwestman.com>
2021-05-31 18:02:12 +0200

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,45 @@ import styled from "styled-components";
 import { StyledContainer } from "../styles/global";
 
 const AppFooter = styled.footer`
-  height: 150px;
+  padding-top: 1rem;
+  padding-bottom: 2rem;
   background-color: rgb(31, 39, 44);
+`;
+const StyledAppFooterContent = styled.footer`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+
+  a {
+    color: white;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Footer = (props) => {
   return (
     <AppFooter>
-      <StyledContainer>footer content</StyledContainer>
+      <StyledContainer>
+        <figure><img src='./images/logo.png' /></figure>
+        <StyledAppFooterContent>
+          <div>
+            <p>Selected series</p>
+            <p>Selected movies</p>
+          </div>
+          <div>
+            <p>Terms and Conditions</p>
+            <p>Integrity and cookies</p>
+          </div>
+          <div>
+            <p>Customer service</p>
+            <p>Activate account</p>
+            <p><a href="mailto:support@comhem.se">support@comhem.se</a></p>
+          </div>
+        </StyledAppFooterContent>
+      </StyledContainer>
     </AppFooter>
   );
 };
